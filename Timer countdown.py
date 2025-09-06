92b4b85def9a9a24d6b69aa2fdcc792b50d17949
Amnoor Brar <TechMan1810@gmail.com>
2025-09-06 14:28:32 +0800

    this is my code for my Timer-countdown.

---
+++ Timer countdown.py
@@ -0,0 +1,14 @@
+# creating a Timer Countdown.
+import time
+#asking the user for the amount of time they want the timer to countdown.
+timer = int(input("timer duration (seconds): "))
+#counting down the amount of number the user said.
+for countdown in range(timer, 0, -1):
+    seconds = countdown % 60
+    minutes = int(countdown / 60) % 60
+    hours = int(countdown / 3600) % 24
+    days = int(countdown / 86400) % 365
+    time.sleep(1)
+    print(f"{days:02}:{hours:02}:{minutes:02}:{seconds:02}")
+#after the countdown I will print "Time's Up!"
+print("Time's Up!")
